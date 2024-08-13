d68fb89f99aca5de17ce1de65ae7c38f160d77b3
Marc Hadfield <marc@vital.ai>
2024-08-13 14:14:30 -0400

    sync

---
+++ kgraphplanner/kgraph_planner.py
@@ -0,0 +1,4 @@
+
+
+class KGraphPlanner:
+    pass
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kgraphplanner',
+    version='0.0.1',
+    author='Marc Hadfield',
+    author_email='marc@vital.ai',
+    description='KGraph Planner',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vital-ai/kgraphplanner',
+    packages=find_packages(exclude=["test"]),
+    entry_points={
+
+    },
+    scripts=[
+
+    ],
+    package_data={
+        '': ['*.pyi']
+    },
+    license='Apache License 2.0',
+    install_requires=[
+        'vital-ai-vitalsigns>=0.1.19',
+        'vital-ai-aimp>=0.1.7',
+        'vital-ai-haley-kg>=0.1.13',
+        'kgraphagent>=0.0.1',
+        'langchain==0.2.12',
+        'langchain-openai==0.1.20'
+    ],
+    extras_require={
+        'dev': [
+            'twine',
+            'setuptools'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
